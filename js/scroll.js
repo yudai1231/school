069d57dbdd0c8d2c53e314eba02739120694982f
Kitano Yudai <>
2020-12-03 01:10:53 +0900

    js追加

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+var navPos = jQuery( '.menu_bar' ).offset().top; // グローバルメニューの位置
+var navHeight = jQuery( '.menu_bar' ).outerHeight(); // グローバルメニューの高さ
+jQuery( window ).on( 'scroll', function() {
+  if ( jQuery( this ).scrollTop() > navPos ) {
+    jQuery( 'body' ).css( 'padding-top', navHeight );
+    jQuery( '.menu_bar' ).addClass( 'm_fixed' );
+  } else {
+    jQuery( 'body' ).css( 'padding-top', 0 );
+    jQuery( '.menu_bar' ).removeClass( 'm_fixed' );
+  }
+});
